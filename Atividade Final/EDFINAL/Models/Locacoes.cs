e5226cdef8140b985b5f6fe38117663a0ac64db9
Renan Matos <49045257+rjsmatos10@users.noreply.github.com>
2021-01-31 00:30:54 -0300

    Add files via upload

---
+++ Atividade Final/EDFINAL/Models/Locacoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EDFINAL.Models
+{
+    class Locacoes
+    {
+        private List<Locacao> contratos;
+        public Locacoes()
+        {
+            contratos = new List<Locacao>();
+        }
+        public List<Locacao> Contratos
+        {
+            get { return contratos; }
+            set { contratos = value; }
+        }
+        public void incluir(Locacao loc)
+        {
+            loc.Id = contratos.Count + 1;
+            contratos.Add(loc);
+        }
+    }
+}
